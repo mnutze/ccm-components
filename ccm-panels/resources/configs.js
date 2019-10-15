826464dff753fb0953364df162aff2b3d0fbd8ec
Michael Nutzenberger <michael.nutzenberger@smail.inf.h-brs.de>
2019-10-15 13:01:36 +0200

    add panels-dashboard-demo config

--- ccm-panels/resources/configs.js
+++ ccm-panels/resources/configs.js
@@ -1,5 +1,9 @@
 ccm.files[ 'configs.js' ]= {
     index2: {
         components: [ 'ccm.component', 'https://ccmjs.github.io/akless-components/blank/ccm.blank.js' ]
+    },
+    dashboard: {
+        css: [ 'ccm.load', 'https://mnutze.github.io/ccm-components/ccm-panels/resources/default.css' ],
+        components: [ 'ccm.component', '../ccm-dashboard/ccm.dashboard.js' ],
     }
 };
